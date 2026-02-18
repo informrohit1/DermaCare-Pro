ea51b4d0c745a418ed5f31a262e7c711ec3c8d17
Rohit Kumar <informrohit1@gmail.com>
2026-02-18 15:13:10 +0530

    changing port address

--- app.py
+++ app.py
@@ -260,4 +260,7 @@ def about_us():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    # app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
